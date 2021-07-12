a9c80416102645cee70bb2d7e4388195e537d0fc
shuruq-alshahrani3 <85189223+shuruq-alshahrani3@users.noreply.github.com>
2021-07-13 00:29:49 +0300

    Update directiones with hardware.php

--- directiones with hardware.php
+++ directiones with hardware.php
@@ -1,8 +1,21 @@
 <?php
+// معلومات الإتصال بقاعدة البيانات
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "control_arm";
+
+// إنشاء الإتصال
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// إختبار الإتصال
+if (!$conn) {
+die("Connection failed: " . mysqli_connect_error());
+}
 $read_data = $conn->query("SELECT * FROM directions");
 foreach($read_data as $data)
 {
-  echo "Direction: " . $data["direction"];
+  echo "<br><br> Direction:<br> " . $data["direction"];
   
 }
 ?>
